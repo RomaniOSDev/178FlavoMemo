e5915e011863c947c0f77b68182a5f4a2d3af285
Roman <romazaniosdev@gmail.com>
2026-05-31 12:27:39 +0300

    Initial Commit

---
+++ 178FlavoMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  178FlavoMemo
+//
+//  Created by Roman on 5/31/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
